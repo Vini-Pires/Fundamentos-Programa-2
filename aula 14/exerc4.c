b915f58741de830cf6bf90e46a801d07fb7d6c62
Vinicius Pires <vini.abpires44@gmail.com>
2023-08-17 17:53:43 -0300

    feat: :construction: Aula 14 - Exercicio
    Exercicio não finalizado

---
+++ aula 14/exerc4.c
@@ -0,0 +1,9 @@
+/*
+  4) Faça um programa que controla o consumo de energia dos
+  eletrodomésticos de uma casa e:
+  - Crie e leia 5 eletrodomésticos que contém nome (máximo 15 letras),
+  potência (real, em kW) e tempo ativo por dia (real, em horas).
+  - Leia um tempo t (em dias), calcule e mostre o consumo total na casa e
+  o consumo relativo de cada eletrodoméstico (consumo/consumo total)
+  nesse período de tempo. Apresente este último dado em porcentagem.
+*/
