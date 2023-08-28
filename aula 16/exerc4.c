83f436b2d13f442eddfc4d41bac59e5265c89c7a
Vini Pires - UTFPR <viniciuspires@alunos.utfpr.edu.br>
2023-08-28 15:21:37 -0300

    feat: :sparkles: Aula 16 - Exercicio

--- aula 16/exerc4.c
+++ aula 16/exerc4.c
@@ -6,3 +6,44 @@
     | [5 - 7[ => C |
     | [0 - 5[ => D |
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  float mediaFinal;
+  char conceito;
+} Aluno;
+
+char Conceitualizacao (float nota) {
+  char conceito;
+  if (nota <= 10.0 && nota >= 9.0) {
+    conceito = 'A';
+  } else if (nota >= 7.0 && nota < 9.0) {
+    conceito = 'B';
+  } else if (nota >= 5.0 && nota < 7.0) {
+    conceito = 'C';
+  } else if (nota >= 0.0 && nota < 5.0) {
+    conceito = 'D';
+  } else {
+    conceito = 'F';
+  }
+
+  return conceito;
+}
+
+int main() {
+  Aluno aluno;
+  char tabelaNota[] = "Notas";
+  char tabelaConceito[] = "Conceito";
+
+  printf("Informe a media final: ");
+  scanf("%f", &aluno.mediaFinal);
+
+  aluno.conceito = Conceitualizacao(aluno.mediaFinal);
+
+  printf("| %-7s | %-10s |\n", tabelaNota, tabelaConceito);
+  printf("| %-7.2f | %-10c |\n", aluno.mediaFinal, aluno.conceito);
+
+  return 0;
+}
