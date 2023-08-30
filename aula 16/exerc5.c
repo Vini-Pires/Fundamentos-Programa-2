7a6b053f1fdd24d643e5fc14458a2491eb6bcbfa
Vinicius Pires <vini.abpires44@gmail.com>
2023-08-29 21:59:59 -0300

    feat: :sparkles: Aula 16 - Exercicio

--- aula 16/exerc5.c
+++ aula 16/exerc5.c
@@ -2,3 +2,29 @@
   5) Crie uma função que que receba o valor de um inteiro
   positivo N, calcule e retorne o fatorial desse número.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * @brief Calcula o fatorial de um numero multiplicando seus antecessores até
+ * que chegue no numero inteiro inicial
+ * 
+ * @param fator um numero do conjunto dos naturais
+ * @return int - retorna a multiplicação de seus antecessores
+ */
+int Fatorial (int fator) {
+  int calculo = 1 ;
+  for (int i = 1; i <= fator; i++) {
+    calculo *= i;
+  }
+  
+  return calculo;
+}
+
+int main () {
+  printf("%d\n", Fatorial(5));
+
+  return 0;
+}
