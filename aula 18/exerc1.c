9284e665d3392273f437c311f8b7a28d6b007adc
Vinicius Pires <vini.abpires44@gmail.com>
2023-09-11 10:49:45 -0300

    chore: :construction: Aula 18 - Exercicio

---
+++ aula 18/exerc1.c
@@ -0,0 +1,8 @@
+/*
+  1) Crie uma função que retorne o fatorial de um número passado por
+  parâmetro. A ideia do fatorial está abaixo:
+    fatorial = {1 se x = 0 | n • fatorial(n - 1) se n > 0}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
