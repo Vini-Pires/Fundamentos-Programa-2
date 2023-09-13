3e6631cc51695bee7e027b82e30e08f99678eac1
Vinicius Pires <vini.abpires44@gmail.com>
2023-09-13 17:22:58 -0300

    chore: :construction: Aula 18 - Exercicio

---
+++ aula 18/exerc4.c
@@ -0,0 +1,21 @@
+/*
+  4) Faça uma função recursiva que retorne o n-ésimo termo da sequência de
+  Fibonacci, sendo que n é recebido por parâmetro. Utilize essa função para
+  desenvolver um programa que mostre no main() os n termos dessa
+  sequência na tela, a partir do valor de n recebido pelo teclado. 
+  Sabe-se que o 1º termo é 0 e o 2º termo é 1.
+*/
+// Sequencia de Fibonacci
+// 0 1 1 2 3 5 8 13 21 ...
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Fibonacci (int termo) {
+  if (termo == 0) {
+    return termo;
+  } else {
+
+  }
+
+}
---
+++ aula 18/exerc5.c
@@ -0,0 +1,9 @@
+/*
+  5) Um problema típico em ciência da computação consiste em
+  converter um número da sua forma decimal para binária. Crie um
+  algoritmo recursivo para resolver esse problema.
+  - Solução trivial: x=0 quando o número inteiro já foi convertido para
+  binário
+  - Passo da recursão: saber como x/2 é convertido. Depois, imprimir um
+  dígito (0 ou 1) dado o sucesso da divisão.
+*/
