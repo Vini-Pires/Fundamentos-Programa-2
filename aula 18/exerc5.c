2ccfa43b352720f676041d82065142adc8391e40
Vinicius Pires <vini.abpires44@gmail.com>
2023-09-14 09:46:45 -0300

    feat: :construction: Aula 18 - Exercicio
    Falta coletar o valor do usuario e verificar se é possivel colocar os bits como uma constante

--- aula 18/exerc5.c
+++ aula 18/exerc5.c
@@ -7,3 +7,38 @@
   - Passo da recursão: saber como x/2 é convertido. Depois, imprimir um
   dígito (0 ou 1) dado o sucesso da divisão.
 */
+
+// Para obtermos o binário de um numero decimal, precisamos
+// dividir o decimal por 2 indefinidamente, após isso começamos a exibir
+// os restos dessas divisões até que a função termine sua execução
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void TransformaBinario (int bits, int inteiro) {
+  int resto = 0;
+  if (inteiro <= 31 && bits > 0){
+    if (inteiro == 0) {
+      TransformaBinario(bits - 1, inteiro);
+      printf("%d", inteiro);
+    } else if (inteiro == 1) {
+      TransformaBinario(bits - 1, 0);
+      printf("%d", inteiro);
+    } else {
+      resto = inteiro % 2;
+      TransformaBinario(bits - 1, inteiro / 2);
+      printf("%d", resto);
+    }
+  } else if (bits > 0){
+    TransformaBinario(bits, 31);
+    printf(" ");
+    TransformaBinario(bits, inteiro - 31);
+  }
+}
+
+int main() {
+  TransformaBinario(8, 300);
+  printf("\n");
+
+  return 0;
+}
